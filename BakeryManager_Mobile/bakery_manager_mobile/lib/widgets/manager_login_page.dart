816b1f605c7e28953bb2623963c6529c862c83f8
Wes Talley <wesatalley@gmail.com>
2024-08-28 22:11:54 -0500

    Started Home Page / Added Color Theme to main.dart

---
+++ BakeryManager_Mobile/bakery_manager_mobile/lib/widgets/manager_login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ManagerLoginPage extends StatefulWidget {
+  const ManagerLoginPage({super.key});
+
+  @override
+  State<ManagerLoginPage> createState() => _ManagerLoginPageState();
+}
+
+class _ManagerLoginPageState extends State<ManagerLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // UI goes here :)
+        );
+  }
+}
