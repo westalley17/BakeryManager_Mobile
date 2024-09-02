fa518e51b9c0ed32c986623f55324719cc0c7bb8
addison goforth <addison@Addisons-MacBook-Pro.local>
2024-09-02 11:19:48 -0500

    Adding Employee and Manager Login Pages

--- BakeryManager_Mobile/bakery_manager_mobile/lib/widgets/manager_login_page.dart
+++ BakeryManager_Mobile/bakery_manager_mobile/lib/widgets/manager_login_page.dart
@@ -1,37 +1,104 @@
+import 'package:bakery_manager_mobile/widgets/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class ManagerLoginPage extends StatefulWidget {
+class ManagerLoginPage extends StatelessWidget {
   const ManagerLoginPage({super.key});
-
-  @override
-  State<ManagerLoginPage> createState() => _ManagerLoginPageState();
-}
-
-class _ManagerLoginPageState extends State<ManagerLoginPage> {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // placeholder code, make this look good
       body: Container(
         color: Theme.of(context).primaryColor,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: usernameController,
+              // baker hat added again cause I liked it here :)
+              Container(
+                padding: const EdgeInsets.fromLTRB(40.0, 0, 0, 0),
+                width: 200.0,
+                height: 200.0,
+                child: Image.asset('assets/images/bakerHat.png'),
               ),
-              TextField(
-                controller: passwordController,
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 60.0, 0, 10.0),
+                child: Text(
+                  "Manager Login",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Return to Home Page"),
+              const Divider(
+                height: 10,
+                thickness: 0.5,
+                indent: 25,
+                endIndent: 25,
+                color: Colors.black,
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 25.0, 20, 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 10.0, 20, 20.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+                child: SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle login logic here
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                        Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 15.0, 0, 0),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "Back to Home",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
